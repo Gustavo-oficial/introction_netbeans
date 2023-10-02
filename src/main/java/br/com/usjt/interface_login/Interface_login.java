6f83a9b4b3937611622f9239e67de7e0e61ee671
Gustavo da Silva Felix <gmandelinha@gmail.com>
2023-10-02 20:18:02 -0300

    primeira interface netbeans

---
+++ src/main/java/br/com/usjt/interface_login/Interface_login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.usjt.interface_login;
+
+/**
+ *
+ * @author 823116200
+ */
+public class Interface_login {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
